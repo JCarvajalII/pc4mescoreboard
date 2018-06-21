0db529093d423175d8aa507a78621daf107a2d78
Jaime Alejandro A. Carvajal II <2161046@slu.edu.ph>
2018-06-22 07:42:03 +0800

    updated

--- sample/js/index.js
+++ sample/js/index.js
@@ -1,36 +1,10 @@
-var GOOD_GRADE = 100;
-var OPEN_TH = 15;
-var PICKUP_TH = 15;
-var OVERDUE_TH = 0;
-var RMA_TH = 5;
-var OP_TH = 5;
+var openT_var = document.getElementById('OPENTicket').innerHTML;
+var link = "html/open.html";
+window.onload = function () {
+    var pickUpTickets = document.getElementById('PICKUPTicket').innerHTML;
+    var orderedPartsTickets = document.getElementById('OPicket').innerHTML;
+    var overdueTickets = document.getElementById('OVERDUETicket').innerHTML;
+    var rmaTickets = document.getElementById('RMATicket').innerHTML;
+    document.getElementById('openHref').setAttribute("href", link);
 
-function setThreshold(value) {
-    document.getElementById('opentthreshold').innerHTML = value;
-}
-
-
-function solveOpen() {
-    var num = 5;
-    var row = document.getElementById('r1');
-    var cell = document.getElementsByTagName('td');
-    //    alert(cell[1].innerHTML);
-    if (num < OPEN_TH) {
-        cell[1].innerHTML = getOpenPercentage(num);
-    }
-}
-
-/*
-FORMULA
-5
-GOOD_GRADE + ((15-5) * 5)
-*/
-function getOpenPercentage(units) {
-    var percentage;
-    percentage = GOOD_GRADE + ((15 - 5) * 5);
-    return percentage + '%';
-}
-
-$(document).ready(function () {
-    $('#myTable').DataTable();
-});
+};
